3c872d03977cd4e7609291746825a96d5c84fd6a
evan <evan@example.com>
2019-10-10 14:14:38 +0800

    run in jetbrain

--- src/genericity/CollectionGenFooDemo.java
+++ src/genericity/CollectionGenFooDemo.java
@@ -5,8 +5,8 @@ import java.util.Collection;
 
 public class CollectionGenFooDemo {
     public static void main(String args[]) {
-        CollectionGenFoo<ArrayList> listFoo = null;
-        listFoo = new CollectionGenFoo<ArrayList>(new ArrayList());
+        CollectionGenFoo<ArrayList<?>> listFoo = null;
+        listFoo = new CollectionGenFoo<ArrayList<?>>(new ArrayList());
         // 出错了,不让这么干。
         // 原来作者写的这个地方有误，需要将listFoo改为listFoo1
         // CollectionGenFoo<Collection> listFoo1 = null;
@@ -15,7 +15,7 @@ public class CollectionGenFooDemo {
         // 现在不会出错了
         // 原来作者写的这个地方有误，需要将listFoo改为listFoo1
         CollectionGenFoo<? extends Collection> listFoo1 = null;
-        listFoo1 = new CollectionGenFoo<ArrayList>(new ArrayList());
+        listFoo1 = new CollectionGenFoo<ArrayList<?>>(new ArrayList());
 
         System.out.println("实例化成功!");
     }
